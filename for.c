7a02c8f61d6c2cc4597a02e7b645bcb9929f8cbf
Ramineniusha9 <usharamineni14@gmail.com>
2024-11-13 11:12:56 +0530

    Add files via upload

--- for.c
+++ for.c
@@ -1,9 +1,8 @@
-#include<stdio.h>
-void main()
-{
-   int  f=1;
-    for(int i=5;i>=1;i--){
-         f=f*i;
-     } printf("%d",f);
+#include <stdio.h>
 
+int main() {
+    // Write C code here
+    printf("Try programiz.pro");
+
+    return 0;
 }
